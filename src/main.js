de7ea4c7dbb009747c13b77d1e9ca4bfc08e0125
x <abhishek29112005@gmail.com>
2025-06-28 09:47:45 +0530

    main container added

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// const mainMid = document.querySelector('.main-mid');
+
+// mainMid.addEventListener('click', async () => {
+//   const response = await fetch('install.html');
+//   const text = await response.text();
+
+//   const tempDiv = document.createElement('div');
+//   tempDiv.innerHTML = text;
+
+//   const installContent = tempDiv.querySelector('.template1');
+//   if (installContent) {
+//     mainMid.appendChild(installContent);
+//   }
+// });
